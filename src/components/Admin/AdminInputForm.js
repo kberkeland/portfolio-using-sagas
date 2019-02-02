88fec571af029358b055d4d8b3efe6195980a929
Kye Berkeland <kyeberkeland@yahoo.com>
2019-02-01 23:14:04 -0600

    Add folders and js files for base project

---
+++ src/components/Admin/AdminInputForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class AdminInputForm extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default AdminInputForm;
---
+++ src/components/Admin/AdminTableItems.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class AdminTableItems extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default AdminTableItems;
